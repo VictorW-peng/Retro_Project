71d8999fb0f4a52d5da2693b9da01e98dd2834df
william <15639667035@163.com>
2022-10-09 14:10:06 +0800

    创建初始项目

---
+++ suture/suture-server/suture-server-solution/src/main/java/com/example/sutureserversolution/SutureServerSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.sutureserversolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutureServerSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SutureServerSolutionApplication.class, args);
+	}
+
+}
---
+++ suture/suture-server/suture-server-solution/src/test/java/com/example/sutureserversolution/SutureServerSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sutureserversolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SutureServerSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
